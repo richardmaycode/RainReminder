e70f233b643004e7bbbd6c1f563a1b1383d6aaef
Richard Wise <richard.wise@hey.com>
2023-06-14 14:27:32 -0400

    Initial Commit

---
+++ RainReminder/RainReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainReminderApp.swift
+//  RainReminder
+//
+//  Created by Richard Wise on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RainReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
